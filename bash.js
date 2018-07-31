e06f217569706561f6a5719195f4e0c5e761224c
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-07-31 11:47:23 -0500

    Solves cat

--- bash.js
+++ bash.js
@@ -1,13 +1,16 @@
 let directory = require('./pwd')
 let file = require('./ls')
+let cat = require('./cat');
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
-  if(cmd.toLowerCase() === 'pwd'){
+  const cmd = data.toString().trim().split(' ');
+  if(cmd[0].toLowerCase() === 'pwd'){
     directory();
   }
-  else if (data.toString().trim() === 'ls') {
+  else if (cmd[0].toLowerCase() === 'ls') {
     file();
+  } else if (cmd[0].toLowerCase() === 'cat') {
+    cat(cmd[1]);
   }
 });
